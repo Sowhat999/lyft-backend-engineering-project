fcfcf1080d06bd0808d1950b2df5ffe0dae2b84b
Joey Laya <joeyaztan@gmail.com>
2022-08-22 19:10:36 -0700

    updated unit tests for engines and batteries

--- battery/base.py
+++ battery/base.py
@@ -1,5 +1,7 @@
 from abc import ABC, abstractmethod
 
+NegativeTimeException = Exception("The current date cannot be before the last service date.")
+
 class Battery(ABC):
   @abstractmethod
   def needs_service():
--- engine/base.py
+++ engine/base.py
@@ -1,5 +1,7 @@
 from abc import ABC, abstractmethod
 
+NegativeMileageException = Exception("The current mileage cannot be less than the last service mileage.")
+
 class Engine(ABC):
   @abstractmethod
   def needs_service():
