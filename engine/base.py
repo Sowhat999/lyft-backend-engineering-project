3065e1f10c1f5c52cc0948ba74ea2d20a7f74d2a
🦴🦷🖕Imre Messiah 🩹🧸🥳😎🤠 <136038418+Sowhat999@users.noreply.github.com>
2024-05-16 05:20:18 -0500

    ko

--- engine/base.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-
-NegativeMileageException = Exception("The current mileage cannot be less than the last service mileage.")
-
-class Engine(ABC):
-  @abstractmethod
-  def needs_service():
-    pass
